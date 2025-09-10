0b5d52e4b7bea6f87b2cf4789660b69c570ac1e5
dim-gggl <dimitri.gaggioli@gmail.com>
2025-09-10 12:36:17 +0200

    Refactoring the whole project

---
+++ src/views/config.py
@@ -0,0 +1,6 @@
+from rich.style import Style
+
+logo_style = Style(color="medium_spring_green", bold=True)
+epic_style = Style(color="orange_red1", bold=True)
+dim_style = Style(color="white", dim=True)
+white = Style(color="grey100", bold=True)
